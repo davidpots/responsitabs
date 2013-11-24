6d35692347477e35d2b55e6deb16c14394bc56af
David Potsiadlo <davidpots@gmail.com>
2013-11-24 17:10:23 -0500

    Makes embedded videos responsive / mobile friendly (max width 100%)

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
+
+  // Nav Drawer Open/Close
   $("#navburger").click(function(){
     $("#drawer").toggle();
     $(this).toggleClass('open');
   });
+
+  // Make embedded videos responsive - via http://webdesignerwall.com/tutorials/css-elastic-videos
+  $('#videos iframe').wrap('<div class="video-wrapper"><div class="video-container"></div></div>')
+  var videos = $('#videos iframe');
+  var size;
+  $.each(videos, function() {
+    size = $(this).attr('width');
+    $(this).parent().parent().css('width',size);
+  } );
+
 });
