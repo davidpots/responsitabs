e9117cd8e6f6054d0f981b66d16c7e5dac967e10
David Potsiadlo <davidpots@gmail.com>
2013-12-03 09:30:19 -0500

    FitText tweaks, drawer nav show/hide tweaks

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,38 @@
+// Add/remove a class based on size of window. Inspired by https://gist.github.com/RyanBrackett/6107983
+// This is used so I can know if a mobile device vs. a desktop device.
+
+          // Change width value on page load
+          $(document).ready(function(){
+              responsive_resize();
+          });
+
+          // Change width value on user resize, after DOM
+          $(window).resize(function(){
+               responsive_resize();
+          });
+
+          function responsive_resize(){
+            var current_width = $(window).width();
+            if(current_width < 1028){
+              $('html').addClass("screen_small").removeClass("scree_large");
+            } 
+            if(current_width > 1028){
+              $('html').addClass("screen_large").removeClass("screen_small");
+              $('#drawer').show();
+            }
+          }
+
+
 $(document).ready(function(){
+
+setTimeout(function(){
+  $('#content').animate({
+    opacity: 1.0,
+    top: "-=20",
+  }, 500);
+},300);
+
+
 
   // Nav Drawer Open/Close
   $("#navburger").click(function(){
@@ -22,7 +56,7 @@ $(document).ready(function(){
 $(window).bind("load", function() {
 
   // Load the Fit Text!
-  $("h1.fit").fitText(1.0, { minFontSize: '64px', maxFontSize: '72px' });
-  $("h2.fit").fitText(1.0, { minFontSize: '30px', maxFontSize: '48px' });
-  $("h3.fit").fitText(1.0, { minFontSize: '27px', maxFontSize: '32px' });
+  $("h1.fit").fitText(0.8, { minFontSize: '64px', maxFontSize: '80px' });
+  $("h2.fit").fitText(1.0, { minFontSize: '30px', maxFontSize: '54px' });
+  $("h3.fit").fitText(0.8, { minFontSize: '27px', maxFontSize: '32px' });
 });
